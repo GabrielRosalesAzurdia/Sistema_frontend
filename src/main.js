eae9c5f6fe1dafb560fbc7a3c1a39341891c4ef6
GabrielRosalesAzurdia <gabrielalejandrorosalesazurdia@gmail.com>
2021-04-11 15:44:19 -0600

    añadiendo bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Notifications from 'vue-notification'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.use(VueAxios, axios)
 Vue.use(Notifications)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <button @click="showToast">Show Toast</button>
+    <button @click="showToast" class="btn btn-info">Show Toast</button>
   </div>
 </template>
 <script>
